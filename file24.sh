fca07f1b3c9fb2f64011c6a1f1aab55a8c496dbf
Raj <neeleshgupta32154@gmail.com>
2026-06-12 18:25:16 +0000

    Added new files

---
+++ file24.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+function welcomeNote {
+      echo "--------"
+      echo "Welcome "
+      echo "--------"
+}
+
+helloWorld() {
+   echo "Hello World"
+}
+
+#To call our function we simply use the function name 
+welcomeNote
+welcomeNote
+welcomeNote
+helloWorld
